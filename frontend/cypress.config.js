ddbceaf1bfbb04cb12069f8bd0e73ac3b76581c0
Gabriel Lopes Weber <gabriellw.grb@gmail.com>
2025-05-31 16:52:18 -0300

    testando

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress"
 import * as dotenv from "dotenv"
 import fs from "fs"
 
-const envFile = `.env.${process.env.CYPRESS_ENV || 'code'}`
+const envFile = `.env.code`
 
 if (fs.existsSync(envFile)) {
   dotenv.config({ path: envFile })
@@ -12,7 +12,7 @@ if (fs.existsSync(envFile)) {
 
 export default defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL,
+    baseUrl: process.env.BASE_URL || 'http://localhost:5173',
     env: {
       VITE_API_URL: process.env.VITE_API_URL
     },
